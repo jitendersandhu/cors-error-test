12caa2dac727bac12d6a03aa8663d971d67bdd66
Jitender <jitendersandhu@users.noreply.github.com>
2016-09-19 12:00:18 -0700

    Update and rename cors-positive-test1.js to cors-positive-test.js

--- cors-positive-test.js
+++ cors-positive-test.js
@@ -1,30 +1,68 @@
 'use strict';
 /* jshint ignore:start */
 var corsTestWithCrossoriginAtrr = (function() {
+    
+    /**
+     * To create reference error
+     *
+     * no @return
+     */
     function refErrorFunc() {
         var tmp = x; //no x variable declared!
     }
-
+    
+    /**
+     * To create custom error
+     *
+     * no @return
+     */
     function sampleFunction() {
         new Function('throw new Error("Custom Error Message")')();
     }
-
+    
+    /**
+     * To create range error
+     *
+     * no @return
+     */
     function rangeErrorFunc() {
         var b = new Array(-1); //range error
     }
-
+    
+    /**
+     * To create stack error
+     *
+     * @param  {Boolean} corsError
+     * @param  {Boolean} crossOriginAttr
+     * no @return
+     */
     function stackErrorFunc(corsError, crossOriginAttr) {
         file1.stackErrorFunc(corsError, crossOriginAttr);
     }
-
+    
+    /**
+     * To create type error
+     *
+     * no @return
+     */
     function typeErrorFunc() {
         document.body.filters[0].apply();
     }
-
+    
+    /**
+     * To create URI error
+     *
+     * no @return
+     */
     function URIErrorfunc() {
         decodeURIComponent('%');
     }
-
+    
+    /**
+     * To create eval error
+     *
+     * no @return
+     */
     function evalErrorFunc() {
         eval('eval("FAIL ")');
     }
